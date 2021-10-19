eb17bf0d79ab89a5fcb20007d55eaae5ede42cae
Deumek <53879371+Deumek@users.noreply.github.com>
2021-10-18 19:57:50 -0500

    cambios a verificar integridad

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -20,8 +20,11 @@ public class Auto {
     }
     public String verificarIntegridad() {
         for(int i = 0; i < asientos.length; i++) {
-            if(asientos[i] != null && asientos[i].registro != registro)
-                return "Las piezas no son originales";
+            if(asientos[i] != null) {
+                if(asientos[i].registro != registro) {
+                    return "Las piezas no son originales";
+                }
+            }
         }
         if(motor.registro != registro)
             return "Las piezas no son originales";
